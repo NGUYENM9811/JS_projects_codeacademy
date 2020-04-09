789d5a766a29d3574154cc1131c37203c795b783
MichaelNguyen <63382783+NGUYENM9811@users.noreply.github.com>
2020-04-09 19:02:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let currentRoundNumber = 1;
 // Write your code below:
 const generateTarget=()=>{
     return Math.floor(10*Math.random());
-    }
-    
+}
+   
 function compareGuesses(computerGuess,userGuess,targetGuess){
     const userDiff=Math.abs(userGuess-targetGuess);
     const computerDiff=Math.abs(computerGuess-targetGuess);
@@ -16,9 +16,8 @@ function compareGuesses(computerGuess,userGuess,targetGuess){
     else {
         return false;
     }
-}
-    
- function updateScore(winner){
+}    
+function updateScore(winner){
     if (winner===‘human’){
         humanScore=humanScore+1;
     }
